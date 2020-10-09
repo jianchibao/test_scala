ae52a1417f5d34339966a2660a15d1ed1ab16f3c
atguigu <2571879875@qq.com>
2020-10-09 22:53:53 +0800

    hot

--- ttt/src/main/java/Hello.java
+++ ttt/src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("gitttttttttttttttt");
         System.out.println("hottttttttttttttttttt");
+        System.out.println("hottttttttttttttttttt");
+        System.out.println("hottttttttttttttttttt");
     }
 }
